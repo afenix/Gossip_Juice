5183c9c7c6e5e98e0ab9dafbec81b7779d7d99dc
Alister A. Fenix <a.fenix@icloud.com>
2015-03-02 21:20:25 -0800

    Add home page elements and navbar

--- router.js
+++ router.js
@@ -1,5 +1,8 @@
 Gossip.Router.map(function() {
-	this.resource('home');
-	this.resource('about');
-	this.resource('contact');
+	this.resource('application')
+	this.resource('home', {path: '/'});
+	this.resource('royal-farce');
+	this.resource('bieber-loves-rogen');
+	this.resource('black-is-back');
+	this.resource('cat-sabotage');
 });
